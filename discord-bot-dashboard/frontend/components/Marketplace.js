25b20246a21c58a2b25893a6e6bcd6effc41c1c7
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-06-25 14:12:31 +0530

    Added: `discord-bot-dashboard/frontend/components/Marketplace.js`

---
+++ discord-bot-dashboard/frontend/components/Marketplace.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Box, Text } from '@chakra-ui/react';
+
+const Marketplace = () => {
+  return (
+    <Box>
+      <Text>Welcome to the Marketplace!</Text>
+    </Box>
+  );
+};
+
+export default Marketplace;
