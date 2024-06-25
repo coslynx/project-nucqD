fc966703aae72b4579c6260acab6fbeacf4fe320
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-06-25 14:12:57 +0530

    Added: `discord-bot-dashboard/frontend/enhancements/StatisticsPage.js`

---
+++ discord-bot-dashboard/frontend/enhancements/StatisticsPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const StatisticsPage = () => {
+  // Add your logic for the statistics page here
+
+  return (
+    <div>
+      <h1>Statistics Page</h1>
+      {/* Add your statistics display components here */}
+    </div>
+  );
+};
+
+export default StatisticsPage;
